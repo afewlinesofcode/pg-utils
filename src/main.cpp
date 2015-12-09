7500aa69fa84c669e332a41d7dbae50a31e98603
Stanislav Yaranov <stanislav.yaranov@tatar.ru>
2015-12-09 17:55:07 +0300

    desired

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,19 @@ int main(int argc, char* argv[]) {
 	{
 		using _ = ys::string::basic_expr;
 
-		std::cout << (_{"trackerid"} == _{13729} && _{"drift"} == _{0} && _{"datetime"} < _{"current_timestamp - interval '1' week"}).str() << std::endl;
+		std::cout << (_{ "trackerid" } == _{ 13729 } && _{ "drift" } == _{ 0 } && _{ "datetime" } < _{
+				"current_timestamp - interval '1' week" }).str() << std::endl;
+
+		// desired usage
+		q.where() & _exists{
+				_select{ "dummy name" } &
+						_from{ "transport.stoptypes1" } &
+						_columns{ "stoptypes1.id" } &
+						_where{
+								(_{ "stoptypes1.stopid" } == _{ "stops.id" }) &&
+										(_{ "stoptypes1.type1id" } == _{ "any($types1Id::int[])" })
+						}
+		};
 	}
 
 	{
