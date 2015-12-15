c6791cce9be261c059a1bef322549a1ae18f8c1c
Stanislav Yaranov <stanislav.yaranov@gmail.com>
2015-12-16 00:55:57 +0300

    query builder

--- include/ys/pgu/query/builder.h
+++ include/ys/pgu/query/builder.h
@@ -12,141 +12,139 @@
 #include <string>
 #include <sstream>
 
-//#include <ys/pgu/query/columns.h>
-//#include <ys/pgu/query/groupby.h>
-//#include <ys/pgu/query/having.h>
-//#include <ys/pgu/query/limit.h>
-//#include <ys/pgu/query/offset.h>
-//#include <ys/pgu/query/orderby.h>
-//#include <ys/pgu/query/relations.h>
-//#include <ys/pgu/query/where.h>
+#include <ys/pgu/query/columns.h>
+#include <ys/pgu/query/groupby.h>
+#include <ys/pgu/query/having.h>
+#include <ys/pgu/query/limit.h>
+#include <ys/pgu/query/offset.h>
+#include <ys/pgu/query/orderby.h>
+#include <ys/pgu/query/relations.h>
+#include <ys/pgu/query/where.h>
 
 #include <ys/traits.h>
 #include <ys/pgu/query/types.h>
+#include <ys/basic_expr.h>
 
 namespace ys {
 namespace pgu {
 namespace query {
 
 template<type Type>
-class builder {
+class builder: public ys::basic_expr<builder<Type>> {
 public:
 	const int type = Type;
 
-	builder(const std::string& relation) {
-//		this->relation(relation);
+	builder(const std::string& relations) {
+		_relations &= relations;
 	}
 
-	/*query::relations& relation() {
-		return _relation;
-	}
-
-	builder& relation(const relations& relation) {
-		_relation = relation;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::none_of(Type, query::remove), query::columns&>>
-	typename T::type columns() {
+	template<typename T = typename std::enable_if<ys::none_of(Type, query::remove), query::columns>>
+	typename T::type& columns() {
 		return _columns;
 	}
 
-	template<typename T = typename std::enable_if<ys::none_of(Type, query::remove), builder&>>
-	typename T::type columns(const query::columns& expr) {
-		_columns = expr;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::none_of(Type, query::insert), query::where&>>
-	typename T::type where() {
-		return _where;
-	}
-
-	template<typename T = typename std::enable_if<ys::none_of(Type, query::insert), builder&>>
-	typename T::type where(const query::where& expr) {
-		_where = expr;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::groupby&>>
-	typename T::type groupby() {
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::groupby>>
+	typename T::type& groupby() {
 		return _groupby;
 	}
 
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), builder&>>
-	typename T::type groupby(const query::groupby& expr) {
-		_groupby = expr;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::having&>>
-	typename T::type having() {
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::having>>
+	typename T::type& having() {
 		return _having;
 	}
 
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), builder&>>
-	typename T::type having(const query::having& expr) {
-		_having = expr;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::orderby&>>
-	typename T::type orderby() {
-		return _orderby;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), builder&>>
-	typename T::type orderby(const query::orderby& expr) {
-		_orderby = expr;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::limit&>>
-	typename T::type limit() {
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::limit>>
+	typename T::type& limit() {
 		return _limit;
 	}
 
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), builder&>>
-	typename T::type limit(const query::limit& count) {
-		_limit = count;
-		return *this;
-	}
-
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::offset&>>
-	typename T::type offset() {
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::offset>>
+	typename T::type& offset() {
 		return _offset;
 	}
 
-	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), builder&>>
-	typename T::type offset(const query::offset& start) {
-		_offset = start;
-		return *this;
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::orderby>>
+	typename T::type& orderby() {
+		return _orderby;
 	}
 
-	template<typename T = typename std::enable_if<Type == query::select, std::string>>
-	typename T::type to_string() const {
-		std::ostringstream s;
+	query::relations& relations() {
+		return _relations;
+	}
 
-		s << "select" << _columns << "from" << _relation <<
-				_where << _groupby << _having << _orderby << _limit << _offset;
+	template<typename T = typename std::enable_if<ys::none_of(Type, query::insert), query::where>>
+	typename T::type& where() {
+		return _where;
+	}
 
-		return s.str();
-	}*/
+	template<typename T = typename std::enable_if<ys::none_of(Type, query::remove), builder>>
+	T::type& operator&=(const query::columns& v) {
+		return append_section(_columns, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::builder>>
+	T::type& operator&=(const query::groupby& v) {
+		return append_section(_groupby, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::builder>>
+	T::type& operator&=(const query::having& v) {
+		return append_section(_having, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::builder>>
+	T::type& operator&=(const query::limit& v) {
+		return append_section(_limit, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::builder>>
+	T::type& operator&=(const query::offset& v) {
+		return append_section(_offset, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::any_of(Type, query::select), query::builder>>
+	T::type& operator&=(const query::orderby& v) {
+		return append_section(_orderby, v);
+	}
+
+	builder& operator&=(const query::relations& v) {
+		return append_section(_relations, v);
+	}
+
+	template<typename T = typename std::enable_if<ys::none_of(Type, query::insert), query::builder>>
+	T::type& operator&=(const query::where& v) {
+		return append_section(_where, v);
+	}
+
+//	template<typename T = typename std::enable_if<Type == query::select, std::string>>
+//	typename T::type to_string() const {
+//		std::ostringstream s;
+//
+//		s << "select" << _columns << "from" << _relations <<
+//				_where << _groupby << _having << _orderby << _limit << _offset;
+//
+//		return s.str();
+//	}
 
 private:
+	template<typename Type>
+	builder& append_section(Type s, Type v) {
+		s &= v;
+		return *this;
+	}
 	/*
 	 * Since std c++11 does not yet support template declarations for variables
 	 * the following ones will exist in every specialization.
 	 */
 
-	/*relations _relation;
+	query::relations _relations;
 	query::columns _columns;
 	query::where _where;
 	query::groupby _groupby;
 	query::having _having;
 	query::orderby _orderby;
 	query::limit _limit;
-	query::offset _offset;*/
+	query::offset _offset;
 };
 
 } /* namespace query */
